40ccb33a2f7bdfbd472ca063dff7376c1831fe12
Obulpathi <obulapathi.challa@rackspace.com>
2014-11-26 12:36:21 -0500

    Adds fig

--- app/app.py
+++ app/app.py
@@ -10,4 +10,4 @@ def hello():
     return 'Hello World! I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
